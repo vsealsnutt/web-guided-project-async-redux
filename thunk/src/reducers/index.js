31d7f1c01c0964bcbaad3c3b031678cb3bd5f541
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-31 12:08:25 -0500

    followed along with guided project video to practice redux-thunk

--- thunk/src/reducers/index.js
+++ thunk/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { SET_IS_FETCHING, GET_PERSON_SUCCESS, GET_PERSON_ERROR } from '../actions';
+
 const initialState = {
   person: {
     name: {
@@ -17,6 +19,12 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_IS_FETCHING:
+      return {...state, isFetching: action.payload};
+    case GET_PERSON_SUCCESS:
+      return {...state, isFetching: false, person: action.payload};
+    case GET_PERSON_ERROR:
+      return {...state, isFetching: false, error: action.payload};
     default:
       return state;
   }
